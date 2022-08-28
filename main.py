87d44dbeb5ac9a7f8c34562cdea2e8062437676b
ekmochi <ewa.kuramochi@gmail.com>
2022-08-28 13:48:41 +0200

    Adding sieve calculation function

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 def get_prime_numbers(n):
-    # todo
-    pass
+    import math
+
+    n_squareroot = int(math.sqrt(n))
+
+    not_prime_numbers = set()
+
+    for increment_check in range(2, n_squareroot+1):
+        for number in range(increment_check+1, n+1):
+            if number%increment_check == 0:
+                not_prime_numbers.add(number)
+                continue
+
+    prime_numbers = list()
+    for number in range(2, n+1):
+        if number not in not_prime_numbers:
+            prime_numbers.append(number)
+
+    return prime_numbers
 
 
 if __name__ == "__main__":
@@ -10,7 +26,6 @@ if __name__ == "__main__":
     while True:
         try:
             max_number = int(input("Please provide a limit to look for prime numbers: \n"))
-
             if max_number <= 2:
                 print("Number must be greater than 2 - in other case there are no prime numbers")
             else:
@@ -18,4 +33,6 @@ if __name__ == "__main__":
         except ValueError:
             print("Please provide a numeric value")
 
-    get_prime_numbers(max_number)
+    prime_numbers = get_prime_numbers(max_number)
+    print(f"There are {len(prime_numbers)} prime numbers less or equal to {max_number}.\nThey are:")
+    [print(i) for i in prime_numbers]
